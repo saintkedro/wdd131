6cea76128d397b7c1221b07f4e5fedd2e1e74899
saintkedro <isongkingsley101@gmail.com>
2026-03-20 23:00:38 +0100

    fixed allignment and display errors

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,15 @@
-const btn = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav ul');
-
-btn.addEventListener('click', () => {
-    nav.classList.toggle('open');
-});
+const menuBtn = document.querySelector('.menu-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const navMenu = document.querySelector('.nav-menu');
 
 const lastModified = document.querySelector('#lastModified');
 
 lastModified.textContent = document.lastModified;
+
+menuBtn.addEventListener('click', () => {
+    navMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+});
